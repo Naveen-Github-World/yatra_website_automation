859a9b3c3deda6ae09677b63537e1c645c119002
Naveen Kumar Thiravida Selvam <naveenselvam1411@gmail.com>
2025-04-21 18:11:24 +0530

    Test

--- PageObjects/landing_page.py
+++ PageObjects/landing_page.py
@@ -69,10 +69,10 @@ class LandingPage:
         self.driver.find_element(By.XPATH, self.yatra_mice_link_xpath).click()
 
     def click_travel_agent_button(self):
-        travel_agent_title_xpath = (WebDriverWait(self.driver, 10).until
-            (EC.element_to_be_clickable(
-            (By.XPATH,"//span[@type='travel-agent']"))))
-        travel_agent_title_xpath.click()
+        # travel_agent_title_xpath = (WebDriverWait(self.driver, 10).until
+        #     (EC.element_to_be_clickable(
+        #     (By.XPATH,"//span[@type='travel-agent']"))))
+        self.driver.find_element(By.XPATH, self.travel_agent_title_xpath).click()
 
 
     def click_login_signup_button(self):
